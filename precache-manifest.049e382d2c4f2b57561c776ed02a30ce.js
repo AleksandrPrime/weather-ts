e34b40dc3e5eb2f63aefc14675c967e9c8a037a2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-07 08:49:14 +0000

    Deploy AleksandrPrime/weather-ts to github.com/AleksandrPrime/weather-ts.git:gh-pages

---
+++ precache-manifest.049e382d2c4f2b57561c776ed02a30ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee5504ae8428acc7939d92250c58bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "413919d16025538f8fa8",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "413919d16025538f8fa8",
+    "url": "/static/js/2.4b1fdfa8.chunk.js"
+  },
+  {
+    "revision": "06e4b88c9037239aa7cc",
+    "url": "/static/js/main.8ac44c10.chunk.js"
+  },
+  {
+    "revision": "c4431a0e499d719a955a",
+    "url": "/static/js/runtime-main.ac6edbd6.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
